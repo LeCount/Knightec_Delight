fd820d4257c7bcfee9b11d02c9712a9ec757bbc4
gRAVEN <stefan.danielsson.1989@gmail.com>
2016-06-18 16:42:36 +0200

    Designing login window and register new user window

--- Simple Social Network/Client/Program.cs
+++ Simple Social Network/Client/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿    using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,15 +8,12 @@ namespace Client
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ClientForm());
+
+            Application.Run(ClientLoginForm.GetForm);
         }
     }
 }
