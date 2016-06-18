a2e35015bdacdc423a4a54aa8028c67f90164ee0
gRAVEN <stefan.danielsson.1989@gmail.com>
2016-06-18 20:55:23 +0200

    The login window now supports event handling, for displaying when the client's IPV4 address is changed

--- Simple Social Network/Client/ClientLoginForm.Designer.cs
+++ Simple Social Network/Client/ClientLoginForm.Designer.cs
@@ -30,12 +30,12 @@
         {
             this.lbl_network = new System.Windows.Forms.Label();
             this.lbl_ip_addr = new System.Windows.Forms.Label();
-            this.btn_new_user = new System.Windows.Forms.Button();
-            this.textBox_password = new System.Windows.Forms.TextBox();
-            this.textBox_user = new System.Windows.Forms.TextBox();
-            this.lbl_password = new System.Windows.Forms.Label();
-            this.lbl_user = new System.Windows.Forms.Label();
             this.btn_login = new System.Windows.Forms.Button();
+            this.lbl_user = new System.Windows.Forms.Label();
+            this.lbl_password = new System.Windows.Forms.Label();
+            this.btn_new_user = new System.Windows.Forms.Button();
+            this.textBox_user = new System.Windows.Forms.TextBox();
+            this.textBox_password = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // lbl_network
@@ -64,47 +64,17 @@
             this.lbl_ip_addr.TabIndex = 3;
             this.lbl_ip_addr.Text = "Local ip address:";
             // 
-            // btn_new_user
+            // btn_login
             // 
-            this.btn_new_user.Location = new System.Drawing.Point(242, 122);
-            this.btn_new_user.Name = "btn_new_user";
-            this.btn_new_user.Size = new System.Drawing.Size(100, 25);
-            this.btn_new_user.TabIndex = 11;
-            this.btn_new_user.Text = "Register new user";
-            this.btn_new_user.UseVisualStyleBackColor = true;
-            this.btn_new_user.Click += new System.EventHandler(this.btn_new_user_Click);
-            // 
-            // textBox_password
-            // 
-            this.textBox_password.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.btn_login.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox_password.Location = new System.Drawing.Point(388, 34);
-            this.textBox_password.Name = "textBox_password";
-            this.textBox_password.Size = new System.Drawing.Size(129, 20);
-            this.textBox_password.TabIndex = 8;
-            // 
-            // textBox_user
-            // 
-            this.textBox_user.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox_user.Location = new System.Drawing.Point(253, 34);
-            this.textBox_user.Name = "textBox_user";
-            this.textBox_user.Size = new System.Drawing.Size(129, 20);
-            this.textBox_user.TabIndex = 7;
-            // 
-            // lbl_password
-            // 
-            this.lbl_password.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.lbl_password.AutoSize = true;
-            this.lbl_password.Location = new System.Drawing.Point(385, 12);
-            this.lbl_password.Name = "lbl_password";
-            this.lbl_password.Size = new System.Drawing.Size(53, 13);
-            this.lbl_password.TabIndex = 10;
-            this.lbl_password.Text = "Password";
+            this.btn_login.Location = new System.Drawing.Point(278, 9);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(53, 42);
+            this.btn_login.TabIndex = 18;
+            this.btn_login.Text = "Log in";
+            this.btn_login.UseVisualStyleBackColor = true;
             // 
             // lbl_user
             // 
@@ -112,36 +82,65 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.lbl_user.AutoSize = true;
-            this.lbl_user.Location = new System.Drawing.Point(250, 12);
+            this.lbl_user.Location = new System.Drawing.Point(12, 9);
             this.lbl_user.Name = "lbl_user";
             this.lbl_user.Size = new System.Drawing.Size(29, 13);
-            this.lbl_user.TabIndex = 9;
+            this.lbl_user.TabIndex = 15;
             this.lbl_user.Text = "User";
             // 
-            // btn_login
+            // lbl_password
             // 
-            this.btn_login.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.lbl_password.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.btn_login.Location = new System.Drawing.Point(523, 12);
-            this.btn_login.Name = "btn_login";
-            this.btn_login.Size = new System.Drawing.Size(49, 42);
-            this.btn_login.TabIndex = 12;
-            this.btn_login.Text = "Log in";
-            this.btn_login.UseVisualStyleBackColor = true;
+            this.lbl_password.AutoSize = true;
+            this.lbl_password.Location = new System.Drawing.Point(147, 9);
+            this.lbl_password.Name = "lbl_password";
+            this.lbl_password.Size = new System.Drawing.Size(53, 13);
+            this.lbl_password.TabIndex = 16;
+            this.lbl_password.Text = "Password";
+            // 
+            // btn_new_user
+            // 
+            this.btn_new_user.Location = new System.Drawing.Point(121, 114);
+            this.btn_new_user.Name = "btn_new_user";
+            this.btn_new_user.Size = new System.Drawing.Size(100, 25);
+            this.btn_new_user.TabIndex = 17;
+            this.btn_new_user.Text = "Register new user";
+            this.btn_new_user.UseVisualStyleBackColor = true;
+            // 
+            // textBox_user
+            // 
+            this.textBox_user.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_user.Location = new System.Drawing.Point(15, 31);
+            this.textBox_user.Name = "textBox_user";
+            this.textBox_user.Size = new System.Drawing.Size(122, 20);
+            this.textBox_user.TabIndex = 13;
+            // 
+            // textBox_password
+            // 
+            this.textBox_password.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_password.Location = new System.Drawing.Point(150, 31);
+            this.textBox_password.Name = "textBox_password";
+            this.textBox_password.Size = new System.Drawing.Size(122, 20);
+            this.textBox_password.TabIndex = 14;
             // 
             // ClientLoginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(584, 261);
+            this.ClientSize = new System.Drawing.Size(340, 261);
             this.Controls.Add(this.btn_login);
             this.Controls.Add(this.lbl_user);
             this.Controls.Add(this.lbl_password);
             this.Controls.Add(this.btn_new_user);
             this.Controls.Add(this.textBox_user);
-            this.Controls.Add(this.lbl_ip_addr);
             this.Controls.Add(this.textBox_password);
+            this.Controls.Add(this.lbl_ip_addr);
             this.Controls.Add(this.lbl_network);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "ClientLoginForm";
@@ -156,12 +155,12 @@
 
         private System.Windows.Forms.Label lbl_network;
         private System.Windows.Forms.Label lbl_ip_addr;
-        private System.Windows.Forms.Button btn_new_user;
-        private System.Windows.Forms.TextBox textBox_password;
-        private System.Windows.Forms.TextBox textBox_user;
-        private System.Windows.Forms.Label lbl_password;
-        private System.Windows.Forms.Label lbl_user;
         private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Label lbl_user;
+        private System.Windows.Forms.Label lbl_password;
+        private System.Windows.Forms.Button btn_new_user;
+        private System.Windows.Forms.TextBox textBox_user;
+        private System.Windows.Forms.TextBox textBox_password;
     }
 }
 
--- Simple Social Network/Client/ClientLoginForm.cs
+++ Simple Social Network/Client/ClientLoginForm.cs
@@ -9,12 +9,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net.NetworkInformation;
+using System.Net;
+using System.Net.Sockets;
 
 namespace Client
 {
     public partial class ClientLoginForm : Form
     {
         private static ClientLoginForm singletonInstance;
+        delegate void SetTextCallback(string text);
 
         public static ClientLoginForm GetForm
         {
@@ -29,21 +32,67 @@ namespace Client
 
         public ClientLoginForm()
         {
-            
-
             InitializeComponent();
+            initializeComponentValues();
+        }
+
+        public static string GetIP4Address()
+        {
+            string IP4Address = String.Empty;
+
+            foreach (IPAddress IPA in Dns.GetHostAddresses(Dns.GetHostName()))
+            {
+                if (IPA.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    IP4Address = IPA.ToString();
+                    break;
+                }
+            }
+
+            return IP4Address;
+        }
+
+        private void initializeComponentValues()
+        {
+            
+            if (System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
+                this.lbl_network.Text = "Network status: Available";
+            else
+                this.lbl_network.Text = "Network status: Unavailable";
+
             NetworkChange.NetworkAvailabilityChanged += AvailabilityChanged;
+
+            this.lbl_ip_addr.Text = "Client ip address: " + GetIP4Address();
         }
 
         private void AvailabilityChanged(object sender, NetworkAvailabilityEventArgs e)
         {
             if (e.IsAvailable)
-                MessageBox.Show("Network connected!");
+            {
+                updateNetworkAvailabilityText("Network status: Available");
+            }
             else
-                MessageBox.Show("Network disconnected!");
+            {
+                updateNetworkAvailabilityText("Network status: Unavailable");
+            }
         }
+        private void updateNetworkAvailabilityText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.lbl_network.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(updateNetworkAvailabilityText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.lbl_network.Text = text;
+            }
 
-
+            this.lbl_ip_addr.Text = "Client ip address: " + GetIP4Address();
+        }
 
         private void btn_new_user_Click(object sender, EventArgs e)
         {
