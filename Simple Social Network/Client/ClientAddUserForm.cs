9b01275f65ebf725d657a1c78ba51398868b3199
gRAVEN <stefan.danielsson.1989@gmail.com>
2016-06-20 13:47:52 +0200

    something weird going on...

---
+++ Simple Social Network/Client/ClientAddUserForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class ClientAddUserForm : Form
+    {
+        private static ClientAddUserForm singletonInstance;
+
+        public static ClientAddUserForm GetForm
+        {
+            get
+            {
+                if (singletonInstance == null || singletonInstance.IsDisposed)
+                    singletonInstance = new ClientAddUserForm();
+
+                return singletonInstance;
+            }
+        }
+
+        public ClientAddUserForm()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            this.Visible = false;
+            ClientLoginForm.GetForm.Show();
+        }
+    }
+}
