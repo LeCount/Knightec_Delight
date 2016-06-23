28cd9ac79f54ba6aaa0636b63f890d82ccefa439
gRAVEN home <stefan.danielsson.1989@gmail.com>
2016-06-23 13:21:33 +0200

    I can access database and set values!

--- Simple Social Network/Server/ServerDatabase.cs
+++ Simple Social Network/Server/ServerDatabase.cs
@@ -5,14 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SQLite; //http://blog.tigrangasparian.com/2012/02/09/getting-started-with-sqlite-in-c-part-one/
 using System.Data.SqlClient;
+using System.Data;
 
 namespace ServerDBCommunication
 {
     public class ServerDatabase
     {
-        private SqlConnection DBconnection;
+        private SQLiteConnection DBconnection;
         private SQLiteCommand sql_command;
-        private SqlCommand query;
+        private SQLiteCommand query;
 
         public ServerDatabase() { }
 
@@ -23,9 +24,17 @@ namespace ServerDBCommunication
 
         private void Connect(string dbFile)
         {
+            try
+            {
+                DBconnection = new SQLiteConnection("Data Source = serverDB.db;Version=3;");
+                DBconnection.Open();
+            }
+            catch(Exception e)
+            {
+                System.Windows.Forms.MessageBox.Show(e.Message);
+            }
 
-            DBconnection = new SqlConnection(dbFile);
-            DBconnection.Open();
+            
         }
 
         public void Disconnect()
@@ -35,12 +44,14 @@ namespace ServerDBCommunication
 
         public void AddUser(string uName, string pWord)
         {
-            query = new SqlCommand();
+            query = new SQLiteCommand();
             query.Connection = DBconnection;
             query.CommandText = "INSERT INTO Login (Username, Password) VALUES (@Username, @Password)";
 
-            query.Parameters.Add(new SqlParameter("@Username", System.Data.SqlDbType.VarChar).Value = uName);
-            query.Parameters.Add(new SqlParameter("@Password", System.Data.SqlDbType.VarChar).Value = pWord);
+            query.CommandType = CommandType.Text;
+
+            query.Parameters.AddWithValue("@Username", uName);
+            query.Parameters.AddWithValue("@Password",pWord);
             
             query.ExecuteNonQuery();
 
